8d38719a1ca737919ddef9a1a356bf31b0c68b6f
Artur <arturk8791@hotmail.com>
2023-05-30 21:36:52 +0200

    zmiana

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,14 +1,25 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import {Routes, Route, Link} from 'react-router-dom';
+import Home from './menu/home';
 
 function App() {
   return (
+    <>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link className="nav-link" to="/">Strona główna</Link>
+        </li>
+
+      </ul>
+
+    </nav>
     <div class="container">
-      Hello whats up
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </div>
+        Hello whats up
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div></>
   );
 }
 
